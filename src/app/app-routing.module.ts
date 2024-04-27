fa9fe89a0556a59d97e4b690497ae87a9ef57027
Camila-Isabela <camila_isabela_lins@hotmail.com>
2024-04-27 19:22:00 -0300

    Exercício LoadChildren/Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'child1', loadChildren: () => import('./modulo1/child1.module').then(m => m.Child1Module) },
+  { path: 'child2', loadChildren: () => import('./modulo2/child2.module').then(m => m.Child2Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
